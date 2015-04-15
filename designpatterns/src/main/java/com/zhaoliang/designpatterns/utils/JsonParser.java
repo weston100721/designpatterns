9e9868132d6133b15b7af27ae1ad8afb7a1e99f4
zhaoliang <zhaoliang@antvision.cn>
2015-04-15 21:00:35 +0800

    [feature] #14 实现json解析的工具类。

---
+++ designpatterns/src/main/java/com/zhaoliang/designpatterns/utils/JsonParser.java
@@ -0,0 +1,14 @@
+package com.zhaoliang.designpatterns.utils;
+
+public class JsonParser {
+
+	public JsonParser() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
