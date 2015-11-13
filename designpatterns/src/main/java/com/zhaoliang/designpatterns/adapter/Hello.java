03d9bc25b0708235ba1340da63bdfd15f0f3bf71
weston <weston_contribute@163.com>
2015-11-13 18:48:32 +0800

    新电脑修改

--- designpatterns/src/main/java/com/zhaoliang/designpatterns/adapter/Hello.java
+++ designpatterns/src/main/java/com/zhaoliang/designpatterns/adapter/Hello.java
@@ -1,11 +1,7 @@
 package com.zhaoliang.designpatterns.adapter;
 
 public class Hello {
-
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello world");
-		System.out.println("hello world");
 	}
-
 }
